5cc4aad10a04d5872a4fe73d2f93503febefd877
Tarun Bhatt <76580985+tarunb155@users.noreply.github.com>
2022-05-27 15:39:00 +0530

    Add files via upload

---
+++ Demo10040_Rest_Controller_Swagger/src/main/java/com/accenture/lkm/Application.java
@@ -0,0 +1,19 @@
+package com.accenture.lkm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
+
+//http://localhost:8090/v2/api-docs
+//http://localhost:8090/swagger-ui.html
+// tell them expand opertions and tell them you can try out fom there it self.. no postman requried
+//http://heidloff.net/article/usage-of-swagger-2-0-in-spring-boot-applications-to-document-apis/
+//http://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
