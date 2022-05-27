f2fb7fff5848de545065351b89da989b1c6b502a
Tarun Bhatt <76580985+tarunb155@users.noreply.github.com>
2022-05-27 13:38:44 +0530

    Add files via upload

---
+++ SpringBootWorkspace/course-api/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,45 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics= new ArrayList<>(Arrays.asList(new Topic("1","Spring","SpringBoot"),new Topic("2","Java","Core Java"), new Topic("3","SQL","PL/SQL")));
+	public List<Topic> getAllTopics()
+	{
+		return topics;
+	}
+	
+	public Topic getTopic(String id)
+	{
+		return topics.stream().filter(t->t.getId().equals(id)).findFirst().get();
+	}
+	public void addTopic(Topic topic)
+	{
+		topics.add(topic);
+	}
+	
+	public void updateTopic(String id, Topic topic)
+	{
+		for( int i=0;i<topics.size();i++)
+		{
+			Topic t=topics.get(i);
+			if(t.getId().equals(id))
+			{
+				topics.set(i,topic);
+				return;
+			}
+		}
+	}
+	
+	public void deleteTopic(String id)
+	{
+		topics.removeIf(t->t.getId().equals(id));
+	}
+	
+}
